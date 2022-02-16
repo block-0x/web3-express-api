a78d6d19279352a40e95fe4fbcc9ee8919d5a172
jun <gum5588@gmail.com>
2022-02-17 06:56:26 +0900

    fix

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 const express= require('express')
 const app = express()
-const routes = require('./routes')
-const Web3 = require('web3');
+const port = 8000;
+app.use(express.json())
+
 const mongodb = require('mongodb').MongoClient
+mongodb.connect(process.env.DB, { useUnifiedTopology: true }, async(err,client) => {
+  client.db('Cluster0')
+})
+
+const Web3 = require('web3');
 const contract = require('truffle-contract');
 const artifacts = require('./build/Inbox.json');
-
-app.use(express.json())
 
 if (typeof web3 !== 'undefined') {
     var web3 = new Web3(web3.currentProvider)
@@ -15,65 +19,79 @@ if (typeof web3 !== 'undefined') {
     var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
 }
 
-// Global variables to represent Database
-var TEST = {
-  'test1': { 'test': 'test'},
-  'test2': { 'test': 'test?'},
-  'test3': { 'test': 'test!'},
-};
-
 async function contractDeploy() {
+  console.log('contractdeploy')
+
   const LMS = contract(artifacts)
   LMS.setProvider(web3.currentProvider)  
-  const accounts = await web3.eth.getAccounts();
   await LMS.deployed();
+}
+
+async function createAccount() {
+  console.log('createAccount')
+
+  const account = await web3.eth.accounts.create();
+  console.log(account)
+  return account
+}
+
+async function getAccounts() {
+  console.log('getAccounts')
+
+  const accounts = await web3.eth.getAccounts();
+  console.log(accounts)
   return accounts
 }
 
-// get test
-app.get('/test', (req, res) => {
+// get: test
+app.post('/test', (req, res) => {
   try {
-    res.status(200).send(TEST);
+    res.status(200).send('hello world!!!');
   } catch(e) {
     res.status(500).send('Error fetching /test');
   }
 });
 
-// post contract deploy
+// post: contract deploy
 app.post('/contractdeploy', (req, res) => {
 
   try {
     res.status(200).send(contractDeploy());
   } catch(e) {
-    res.status(500).send('Error fetching /todos');
+    res.status(500).send('Error fetching /contractdeploy');
   }
 });
 
-// post get nft
-app.post('/contractdeploy', (req, res) => {
+// post: create account
+app.post('/createaccount', (req, res) => {
 
   try {
-    res.status(200).send(contractDeploy());
+    res.status(200).send(createAccount());
   } catch(e) {
-    res.status(500).send('Error fetching /todos');
+    res.status(500).send('Error fetching /createaccount');
   }
 });
 
-// post get nft
-app.post('/contractdeploy', (req, res) => {
+// post: create account
+app.post('/getaccounts', (req, res) => {
 
   try {
-    res.status(200).send(contractDeploy());
+    res.status(200).send(getAccounts());
   } catch(e) {
-    res.status(500).send('Error fetching /todos');
+    res.status(500).send('Error fetching /getaccounts');
   }
 });
 
-mongodb.connect(process.env.DB,{ useUnifiedTopology: true }, async(err,client)=>{
-    const db =client.db('Cluster0')
-    //const lms = LMS.at(contract_address) for remote nodes deployed on ropsten or rinkeby
-    routes(app, db)
-    app.listen(process.env.PORT || 8082, () => {
-        console.log('listening on port '+ (process.env.PORT || 8082));
-     })
-})
+// post: create account
+app.post('/getaccounts', (req, res) => {
+
+  try {
+    res.status(200).send(getAccounts());
+  } catch(e) {
+    res.status(500).send('Error fetching /getaccounts');
+  }
+});
+
+app.listen(port, () => {
+  console.log("Server listening on port " + port);
+});
